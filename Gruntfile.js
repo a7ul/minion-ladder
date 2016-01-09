4d4d51a6d3cf89e3aaaf53f94d279a28d2ad90d4
Atul <atulanand94@gmail.com>
2016-01-10 02:41:42 +0530

    Finished build

--- Gruntfile.js
+++ Gruntfile.js
@@ -341,6 +341,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             '*.html',
+            'audio/*.mp3',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
             'styles/fonts/{,*/}*.*'
